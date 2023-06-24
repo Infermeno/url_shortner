8231ccccecf711beaf351eef6ad8e16e0a40dbef
Infermeno <114936287+Infermeno@users.noreply.github.com>
2023-06-24 11:02:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const { connectToMongoDB } = require("./connect");
+const urlRoute = require("./routes/url");
+const URL = require("./models/url");
+
+const app = express();
+const PORT = 8001;
+
+connectToMongoDB("mongodb://localhost:27017/short-url").then(() =>
+  console.log("Mongodb connected")
+);
+
+app.use(express.json());
+
+app.use("/url", urlRoute);
+
+app.get("/:shortId", async (request, response) => {
+  const shortId = request.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    }
+  );
+  response.redirect(entry.redirect_URL);
+});
+
+app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));

408de7e38b3cb209da0d4dc935fddd5daabddfb3
Infermeno <114936287+Infermeno@users.noreply.github.com>
2023-06-24 11:04:48 +0530

    Create url.js

---
+++ models/url.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const urlSchema = new mongoose.Schema(
+  {
+    shortId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    redirect_URL: {
+      type: String,
+      required: true,
+    },
+    visitHistory: [{ timestamp: { type: Number } }],
+  },
+  { timestamps: true }
+);
+
+const URL = mongoose.model("url", urlSchema);
+
+module.exports = URL;
